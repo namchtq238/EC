48793b493e7bc6d52d101d5cfc841b42893410bd
thinh12 <bmtnt09042000@gmail.com>
2022-03-25 12:38:50 +0700

    update book v1

--- ec/book/models.py
+++ ec/book/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'publisher'
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'author'
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'category'
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    num = models.CharField(max_length=255)
+    language = models.CharField(max_length=255)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    class Meta:
+        db_table = 'book'
